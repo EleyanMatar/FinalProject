b91218385aa70a78fff6f5f5bcb1326c620f5d80
Eleyan <alyan.hani@outlook.com>
2025-05-13 09:53:47 +0300

    create functions to read html and text files, in backend

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 import flask 
 app = flask.Flask('Surveying')
 
+def get_html(html_file):
+    file = open(html_file)
+    content = file.read()
+    file.close()
+    return content
+
+def read_text(text_file):
+    file = get_html(text_file)
+    content = file.split('\n')
+    return content
+
+def text_to_html(text_file):
+    file = read_text(text_file)
+    returned_paragraphs = ''
+    for i in file:
+        returned_paragraphs+= f'<p>{i}</p>'
+    return returned_paragraphs
+
 @app.route('/')
 def homepage():
-    return "this is homepage"
+    return get_html('index.html')
 
 @app.route('/update')
 def update():
