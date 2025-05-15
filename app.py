2be7757a7e7c833b0c241d5ff855e685c7fa19bc
Eleyan <alyan.hani@outlook.com>
2025-05-15 10:23:32 +0300

    Sign in and sign up is finished

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import flask 
 app = flask.Flask('Surveying')
+# flask.session will not work without secret_key()
+app.secret_key = "#secret_key@2025"
+
 
 # create class for new account:
 class account:
@@ -46,13 +49,22 @@ def text_to_html(text_file):
         returned_paragraphs+= f'<p>{i}</p>'
     return returned_paragraphs
 
-# read users file, which contains the usernames and password
-def open_users_file(users_file):
-    users = {}
-    users_file = read_split_text_to_list()
-    for user in users_file:
-        username, password = user.split(":")
-        users[username] = password
+# read users file, which contains objects properties separated with ":" and turn it to dictionary
+def open_users_file(file_name='username.txt'):
+    users = []
+    lines = read_split_text_to_list(file_name)
+    for line in lines:
+        parts = line.strip().split(":")
+        if len(parts) == 6:
+            user = {
+                "id": parts[0],
+                "first_name": parts[1],
+                "last_name": parts[2],
+                "username": parts[3],
+                "password": parts[4],
+                "email_address": parts[5]
+            }
+            users.append(user)
     return users
 
 #homepage, give a brief of the webapp and give him the option to sign in or sign up, then redirect the user for /action route
@@ -63,20 +75,48 @@ def homepage():
 # flask.request.method() to call the method type.
     if flask.request.method == "POST":
         password = flask.request.form["password"]
-        username = flask.request.form["username"]
+        username = flask.request.form["username"].lower()
         users = open_users_file("username.txt")
 #Session, We use it because each request work individually without connected with each other, so the appropriate way to redirect to action route is to make sure that the user sign in successfully.
-        if (username in users.keys()):
-            if users[username]==password:
-                flask.session['username'] = username
-                return flask.redirect('/actions')
-            else:
-                return "Wrong password. Try again."
-        else:
-            return "Wrong username. try again, or press sign up if it is your first time.."
+        for i in users:
+            if i['username'] == username:
+                if i['password'] == password:
+                    flask.session['username'] = username
+                    return flask.redirect('/actions')
+                else:
+                    return flask.render_template("index.html", message="Wrong password. Try again.")
+        return flask.render_template("index.html", message="Wrong username. Try again.")
+
+    return flask.render_template("index.html")
+                
         
-@app.route('/signup', methods = "POST")
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if flask.request.method == 'POST':
+        first_name = flask.request.form["first_name"].capitalize()
+        last_name = flask.request.form["last_name"].capitalize()
+        email_address = flask.request.form["email_address"].lower()
+        username = flask.request.form["username"].lower()
+        password = flask.request.form["password"]
+        id = flask.request.form["id"]
+
+        users = open_users_file("username.txt")
+
+        for user in users:
+            if user['id'] == id:
+                return flask.render_template("signup.html", message="This ID already exists. Try again.")
+            if user['username'] == username:
+                return flask.render_template("signup.html", message="This username is already taken. Try again.")
+            if user['email_address'] == email_address:
+                return flask.render_template("signup.html", message="This email is already registered. Try again.")
+
+        
+
+        account(id,first_name,last_name,username,password,email_address).save_to_file()
+        return "Account created successfully! <a href='/'>Go to login</a>"
+    
+    return flask.render_template('signup.html')
+
 
 
 @app.route('/actions')
@@ -89,9 +129,9 @@ def update():
     return 'this page is for update'
 
 @app.route('/view')
-def add():
-    return 'this page to add new destruction'
+def view():
+    return 'this page to view'
 
 @app.route('/add')
 def add():
-    return 'this page to add new destruction'
+    return 'this page to add new survey'
