62224237f091c965c29da32a48a7d9a7672d49f1
Eleyan <alyan.hani@outlook.com>
2025-05-18 16:42:28 +0300

    Restrictions on input value is created

---
+++ static/show_survey.js
@@ -0,0 +1,26 @@
+function isNumber(text){
+    if (isNaN(Number(text))){
+        return false;
+    } else {
+        return true;
+    }
+}
+function isNineDigitNumber(text){
+    if (isNumber(text)){
+        if (text.length==9){
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+
+document.getElementById('show_survey').addEventListener('submit', function (event){
+    let id = document.getElementsByName('id')[0].value;
+    if (!isNineDigitNumber(id)){
+        event.preventDefault();
+        document.getElementById('message').innerHTML = " ID should be nine digit numbers";
+    }})
